a2e8af8de5a75a7079c7fe19befc8b7871c8ac38
Kohlrabi404 <m02111034@iot.ac.jp>
2023-07-11 14:36:50 +0900

    add pi related functions

--- picapture.py
+++ picapture.py
@@ -1,43 +1,49 @@
 #! /usr/bin/env python3
-#2photosample.py
 
 import picamera
-
+from pathlib import Path
 import time
 #import date
 import RPi.GPIO as GPIO
 
-#GPIO.cleanup()
-PICTURE_WIDTH = 800
-PICTURE_HEIGHT = 600
+class Data :
+	def __init__(self):
+		#GPIO.cleanup()
+		self.PICTURE_WIDTH = 800
+		self.PICTURE_HEIGHT = 600
+		self.INTAVAL = 600
 
-INTAVAL = 600
-value = 817
+		self.SLEEPTIME = 10
+		#sleeptime : time between photos
+		self.CHECKTIME = 10
+		#checktime : still sensor ON?
+		self.WAITTIME = 10
 
-SLEEPTIME = 10
-#sleeptime : time between photos
-CHECKTIME = 10
-#checktime : still sensor ON?
+		SENSOR_PIN = 9
+		GPIO.cleanup()
 
-SENSOR_PIN = 9
-GPIO.cleanup()
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(SENSOR_PIN, GPIO.IN)
+		self.cam = picamera.PiCamera()
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(SENSOR_PIN, GPIO.IN)
-cam = picamera.PiCamera()
+		self.cam.resolution = (self.PICTURE_WIDTH,self.PICTURE_HEIGHT)
+		self.cam.resolution = (640, 480)
+	
+	def get_pic_and_vid(self):
+		while True : 
+			if GPIO.input(self.SENSOR_PIN) == GPIO.HIGH:
+				time.sleep(self.CHECKTIME)
+				if GPIO.input(self.SENSOR_PIN) == GPIO.HIGH:
+					directory = Path.cwd()
+					fname = time.strftime("%Y-%m-%d %H:%M:%S")
+					picname = fname + '.jpg'
+					self.cam.capture(directory/picname)
+					time.sleep(self.SLEEPTIME)
 
-cam.resolution = (PICTURE_WIDTH,PICTURE_HEIGHT)
-cam.resolution = (640, 480)
+					vidname = 'video' + time.strftime("%Y-%m-%d %H:%M:%S") + '.jpg'
+					self.cam.start_recording(directory/vidname)
+					time.sleep(self.WAITTIME)
+					self.cam.stop_recording()
+					time.sleep(self.SLEEPTIME)
 
-st = time.time() - INTAVAL
-i = 0
-
-while True:
-	if GPIO.input(SENSOR_PIN) == GPIO.HIGH:
-		time.sleep(CHECKTIME)
-		if GPIO.input(SENSOR_PIN) == GPIO.HIGH:
-			i =i+1
-			st =time.time()
-			filename = time.strftime("%Y%m%d%H%M%S") + '.jpg'
-			cam.capture('/home/pi/Desktop/%s.jpg' %filename)	
-			time.sleep(SLEEPTIME)
+					return [fname, directory/picname, directory/vidname]
